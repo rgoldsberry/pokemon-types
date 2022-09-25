3a024ea8269bb29d7aa6a1c9d05f534ab9abb9ea
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-09-25 17:30:04 -0600

    built sqlite file and table render on home page

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+import pandas as pd
 from flask import Flask, render_template
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///pkmn.sqlite")
+
+type_df = pd.read_sql("SELECT * FROM typemult", con = engine, index_col="type")
+
+# core function for deciding what to attack with
+def attack_with(type1, type2=None):
+
+    #monotype    
+    if type2 == None:
+        damage = type_df[type1]
+        damage = damage[damage != 1].sort_values(ascending=False)
+        return pd.DataFrame(damage, columns = ["defmult"])
+    #dual type
+    else:
+        damage = type_df[type1] * type_df[type2]
+        damage = damage[damage != 1].sort_values(ascending=False)
+        return pd.DataFrame(damage, columns = ["defmult"])
+
+
+#################################################
+# Flask Routes
+#################################################
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    table = attack_with('water', 'flying')
+    return render_template("index.html", table = table.to_html())
 
 # to-do: add two pages, one for inputting the types, one for searching by pokemon name
 # in either case, send to a calculation route that grabs the columns from a sqlite
@@ -17,6 +48,10 @@ def type_input():
 def name_input():
     return "You came to the name input page!"
 
+
+#################################################
+# Running App
+#################################################
 
 if __name__ == "__main__":
     app.run(debug=True)
