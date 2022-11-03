2f5a137b2f065b23e6b7b32664b298ff755be300
rgoldsberry <107135474+rgoldsberry@users.noreply.github.com>
2022-11-03 11:25:10 -0600

    added error handling and made session variables more efficient

--- web_app/app.py
+++ web_app/app.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = os.urandom(12).hex()
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///web_app/pkmn.sqlite")
+engine = create_engine("sqlite:///pkmn.sqlite")
 
 type_df = pd.read_sql("SELECT * FROM typemult", con = engine, index_col="type")
 
@@ -53,7 +53,14 @@ def index():
 @app.route("/type", methods = ['POST', 'GET'])
 def type_input():
     if request.method == "POST":
-        session.clear()
+        
+        # try clearing out previous variables in the session
+        try:
+            session.pop('send')
+        except:
+            pass
+
+        # look for the monotype indicator, return a single type answer
         if request.form.get == "on":
             type1 = request.form.get("type_1_input")
             def_mult_table = attack_with(type1).to_html(index = False, classes=['table table-striped'])
@@ -63,17 +70,28 @@ def type_input():
                 }
             session['send'] = json.dumps(send)
             return redirect(url_for("calc"))
+        # dual type answer
         else:
             type1 = request.form.get("type_1_input")
             type2 = request.form.get("type_2_input")
-            def_mult_table = attack_with(type1, type2).to_html(index = False, classes=['table table-striped'])
-            send = {
-                "type1": type1,
-                "type2": type2,
-                "def_mult": def_mult_table
+
+            # catch the error of people inputting the same type twice
+            if type1 == type2:
+                send = {
+                    "error": 'Same type input twice. Did you mean to check "Monotype"?',
+                    "source": "/type"
                 }
-            session['send'] = json.dumps(send)
-            return redirect(url_for("calc"))
+                session['send'] = json.dumps(send)
+                return redirect(url_for("oops"))
+            else:
+                def_mult_table = attack_with(type1, type2).to_html(index = False, classes=['table table-striped'])
+                send = {
+                    "type1": type1,
+                    "type2": type2,
+                    "def_mult": def_mult_table
+                    }
+                session['send'] = json.dumps(send)
+                return redirect(url_for("calc"))
     else:
         return render_template("type.html", type_list=type_list)
 
@@ -81,35 +99,57 @@ def type_input():
 
 @app.route("/name", methods=["POST", "GET"])
 def name_input():
-    if request.method == "POST":
-        session.clear()
-        pkmn=request.form.get("poke_name")
+    try:
+        if request.method == "POST":
+            # try clearing out previous variables in the session
+            try:
+                session.pop('send')
+            except:
+                pass
 
-        if name_df.loc[pkmn][1]:
-            type1=name_df.loc[pkmn][0]
-            type2=name_df.loc[pkmn][1]
-            def_mult_table = attack_with(type1, type2).to_html(index = False, classes=['table table-striped'])
-            send = {
-                "pkmn": pkmn,
-                "type1": type1,
-                "type2": type2,
-                "def_mult": def_mult_table
-            }
-            session['send'] = json.dumps(send)
-            return redirect(url_for("calc"))
+            pkmn=request.form.get("poke_name")
+            
+            # see if the pokemon is dual type
+            if name_df.loc[pkmn][1]:
+                type1=name_df.loc[pkmn][0]
+                type2=name_df.loc[pkmn][1]
+                def_mult_table = attack_with(type1, type2).to_html(index = False, classes=['table table-striped'])
+                send = {
+                    "pkmn": pkmn,
+                    "type1": type1,
+                    "type2": type2,
+                    "def_mult": def_mult_table
+                }
+                session['send'] = json.dumps(send)
+                return redirect(url_for("calc"))
+            # monotype
+            else:
+                type1=name_df.loc[pkmn][0]
+                def_mult_table = attack_with(type1).to_html(index = False, classes=['table table-striped'])
+                send = {
+                    "pkmn": pkmn,
+                    "type1": type1,
+                    "def_mult": def_mult_table
+                }
+                session['send'] = json.dumps(send)
+                return redirect(url_for("calc"))
+        # render base page with names as a list for autofill
         else:
-            type1=name_df.loc[pkmn][0]
-            def_mult_table = attack_with(type1).to_html(index = False, classes=['table table-striped'])
-            send = {
-                "pkmn": pkmn,
-                "type1": type1,
-                "def_mult": def_mult_table
-            }
-            session['send'] = json.dumps(send)
-            return redirect(url_for("calc"))
-    else:
-        name_list = list(name_df.index)
-        return render_template("name.html", name_list = name_list)
+            name_list = list(name_df.index)
+            return render_template("name.html", name_list = name_list)
+    # catch the error of inputting a pokemon not in the list
+    except:
+        send = {
+           "error": "I don't know that Pokémon yet, sorry.",
+           "source": "/name"
+        }
+        session['send'] = json.dumps(send)
+        return redirect(url_for("oops"))
+
+@app.route("/oops")
+def oops():
+    received = session['send']
+    return render_template("oops.html", received = json.loads(received))
 
 
 @app.route("/calc")
@@ -122,4 +162,4 @@ def calc():
 #################################################
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
